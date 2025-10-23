db4ee8be6aa37e7606f9d2970ae482704c0404fe
D506n <D506n@yandex.ru>
2025-10-23 15:35:48 +0300

    Добавил хранилище нотификаторов, чтобы получить их из любого места программы по id сессии

--- app/src/shared/notificator/notificator.py
+++ app/src/shared/notificator/notificator.py
@@ -3,6 +3,17 @@ from nicegui import Event
 from logging import getLogger
 from typing import TypedDict
 
+class NotificatorsStorage:
+    storage: dict[str, 'Notificator'] = {}
+
+    @classmethod
+    def add(cls, id:str, notificator: 'Notificator'):
+        cls.storage[id] = notificator
+
+    @classmethod
+    def get(cls, id:str):
+        return cls.storage.get(id)
+
 logger = getLogger(__name__)
 
 class NotifyEventArgsDict(TypedDict):
@@ -29,11 +40,12 @@ class NotifyEventArgsDict(TypedDict):
 NotifyEvent = Event[NotifyEventArgsDict]
 
 class Notificator:
-    def __init__(self):
+    def __init__(self, id:str):
         ui.html('<style>.multi-line-notification { white-space: pre-line; }</style>', sanitize=False) # без этого многострочные уведомления не работают
         self.layout = ui.row() # костыль чтобы нотификатор привязывался к странице
         self.event = NotifyEvent()
         self.event.subscribe(self.sub)
+        NotificatorsStorage.add(id, self)
 
     async def sub(self, e: NotifyEventArgsDict):
         event = NotifyEventArgsDict.default()
